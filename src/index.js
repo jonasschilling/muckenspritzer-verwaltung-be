a98592b82e1ef97389713bf312eb2c793fa93783
Jonas Schilling <jonas.schilling@sap.com>
2024-04-29 14:59:51 +0200

    added function to get ip address for dev

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-const http = require ('http');
-const { config } = require ('dotenv');
-const app = require ('./app.js');
-const logger = require ('./utils/logger.js');
+const http = require('http');
+const { config } = require('dotenv');
+const app = require('./app.js');
+const logger = require('./utils/logger.js');
 
 if (process.env.NODE_ENV !== "production") {
     config();
@@ -13,4 +13,34 @@ const PORT = process.env.PORT || 3003;
 
 server.listen(PORT, () => {
     logger.info(`Server running on port ${PORT}`);
+    getIPForDev();
 });
+
+
+function getIPForDev() {
+
+    const options = {
+        hostname: 'api.ipify.org',
+        port: 80,
+        path: '/',
+        method: 'GET'
+    };
+
+    const req = http.request(options, (res) => {
+        let data = '';
+
+        res.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        res.on('end', () => {
+            logger.info('IP-Adresse der Anwendung:', data);
+        });
+    });
+
+    req.on('error', (error) => {
+        console.error(error);
+    });
+
+    req.end();
+}
